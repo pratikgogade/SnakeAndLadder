01b2a9d1ac586fd6ae12cf5d8f51094569b22b30
unknown <Pratik@Example.com>
2022-11-08 15:02:39 +0530

    [Pratik]Add:Count Of Dice

--- SnakeAndLadder/GamePlay.cs
+++ SnakeAndLadder/GamePlay.cs
@@ -5,7 +5,7 @@ namespace SnakeAndLadderProblem
     {
         Random random = new Random();
         const int NO_PLAY = 0, LADDER = 1, SNAKE = 2, WINNING_POSITION = 100;
-        int playerPosition = 0;
+        int playerPosition = 0, count = 0;
 
         public void Position()
         {
@@ -16,6 +16,7 @@ namespace SnakeAndLadderProblem
             int dice = random.Next(1, 7);
             Console.WriteLine("Output of Dice - " + dice);
             _ = this.playerPosition;
+            count++;
             return dice;
         }
         public void Check()
@@ -44,6 +45,7 @@ namespace SnakeAndLadderProblem
                 }
                 Console.WriteLine("You are at " + this.playerPosition);
             }
+            Console.WriteLine("Total dice count will be - " + count);
             Console.WriteLine("Yuppieee !! You won the game");
         }
     }
